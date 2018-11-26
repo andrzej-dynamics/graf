ecca70e3ae34476daf66488bfedb312a818a6307
kfarbaniec <farbaniec.karol@gmail.com>
2018-11-26 13:30:03 +0100

    file input in progress

--- src/main/java/com/andrzej/dynamics/graf/MainClass.java
+++ src/main/java/com/andrzej/dynamics/graf/MainClass.java
@@ -1,10 +1,30 @@
 package com.andrzej.dynamics.graf;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by jazowski_1150904 on 26.11.18.
  */
 public class MainClass {
-    public static void main(String[] args) {
-        System.out.println("start");
+    public static void main(String[] args) throws IOException {
+//        System.out.println("start");
+        String fileName = "";
+
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        String ls = System.getProperty("line.separator");
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+// delete the last new line separator
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        reader.close();
+        String content = stringBuilder.toString();
+
     }
 }
