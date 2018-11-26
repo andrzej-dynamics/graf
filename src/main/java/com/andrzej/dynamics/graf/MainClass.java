fad54c12a1a093c83cbaed8c209898fb454cf429
andrewPompa <michal.jazowski@student.uj.edu.pl>
2018-11-26 12:52:03 +0100

    first blood

---
+++ src/main/java/com/andrzej/dynamics/graf/MainClass.java
@@ -0,0 +1,10 @@
+package com.andrzej.dynamics.graf;
+
+/**
+ * Created by jazowski_1150904 on 26.11.18.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
