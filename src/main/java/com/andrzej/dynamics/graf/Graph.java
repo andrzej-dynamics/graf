6e41e374e76241cd4b702fce7200c59cb79960ca
mijo <mijo@ailleron.com>
2018-12-10 12:36:10 +0100

    first test added

--- src/main/java/com/andrzej/dynamics/graf/Graph.java
+++ src/main/java/com/andrzej/dynamics/graf/Graph.java
@@ -1,8 +1,22 @@
 package com.andrzej.dynamics.graf;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Graph {
-    private Map<Integer, Node> graph;
+    private final Map<Integer, Node> graph;
 
+    public Graph() {
+        this.graph = new HashMap<Integer, Node>();
+    }
+
+    public void addNode(int nodeIndex, String nodeValue, List<Integer> links) {
+        final Node nodeFromMap = graph.get(nodeIndex);
+        if (nodeFromMap != null) {
+            throw new IllegalArgumentException("node already exists");
+        }
+        final Node node = new Node(nodeValue, links);
+        graph.put(nodeIndex, node);
+    }
 }
