0b6050abaf95697bcbcd034ea9ec62ee6aca91a7
mijo <mijo@ailleron.com>
2018-12-17 09:47:41 +0100

    Finding shortest path in graph added

--- src/main/java/com/andrzej/dynamics/graf/Graph.java
+++ src/main/java/com/andrzej/dynamics/graf/Graph.java
@@ -1,22 +1,37 @@
 package com.andrzej.dynamics.graf;
 
-import java.util.HashMap;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Graph {
-    private final Map<Integer, Node> graph;
-
+    private final org.jgrapht.Graph<Integer, String> directedGraph;
     public Graph() {
-        this.graph = new HashMap<Integer, Node>();
+        directedGraph = new SimpleWeightedGraph<Integer, String>(String.class);
     }
 
     public void addNode(int nodeIndex, String nodeValue, List<Integer> links) {
-        final Node nodeFromMap = graph.get(nodeIndex);
-        if (nodeFromMap != null) {
-            throw new IllegalArgumentException("node already exists");
+        directedGraph.addVertex(nodeIndex);
+        for (int link : links) {
+            directedGraph.addVertex(link);
+            directedGraph.addEdge(nodeIndex, link, nodeIndex + "-" + link + " " + nodeValue);
+            directedGraph.setEdgeWeight(nodeIndex, link, nodeValue.length());
         }
-        final Node node = new Node(nodeValue, links);
-        graph.put(nodeIndex, node);
+    }
+
+    public List<String> shortestPath(int from, int to) {
+        System.out.println("Shortest path from i to c:");
+        DijkstraShortestPath<Integer, String> dijkstra = new DijkstraShortestPath<Integer, String>(directedGraph);
+        final GraphPath<Integer, String> path = dijkstra.getPath(from, to);
+        final List<String> edgeList = new ArrayList<String>();
+        for (String rawEdgeLabel : path.getEdgeList()) {
+            final int splitCharacter = rawEdgeLabel.indexOf(" ");
+            final String edgeLabel = rawEdgeLabel.substring(splitCharacter + 1);
+            edgeList.add(edgeLabel);
+        }
+        return edgeList;
     }
 }
--- src/test/java/com/andrzej/dynamics/graf/GraphTest.java
+++ src/test/java/com/andrzej/dynamics/graf/GraphTest.java
@@ -1,11 +1,27 @@
 package com.andrzej.dynamics.graf;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GraphTest {
 
     @Test
     public void testAddNode() {
-        System.out.println("test");
+        final Graph graph = new Graph();
+        graph.addNode(0, "ADNRZEJ", Collections.singletonList(1));
+        graph.addNode(1, "BREZES", Arrays.asList(2, 3));
+        graph.addNode(2, "XYZW", Collections.singletonList(4));
+        graph.addNode(3, "ABC", Collections.singletonList(4));
+        graph.addNode(4, "123", Collections.<Integer>emptyList());
+
+        final List<String> path = graph.shortestPath(0, 4);
+        Assert.assertEquals(path.size(), 3);
+        Assert.assertEquals(path.get(0), "ADNRZEJ");
+        Assert.assertEquals(path.get(1), "BREZES");
+        Assert.assertEquals(path.get(2), "ABC");
     }
 }
